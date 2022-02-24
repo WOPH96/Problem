123a107cbf0c09eb483d9f45a0bf7b25769e20a8
양원필 <yang-wonphil@Phils-MacBookPro.local>
2022-02-24 18:45:38 +0900

    phone_number

--- python/phone_number/pn.py
+++ python/phone_number/pn.py
@@ -1,1 +1,9 @@
-dd
+def solution(phone_number):
+    answer = ''
+    
+    answer = "*" * (len(phone_number)-4)
+    answer += phone_number[-4:]    
+
+    return answer
+
+print(solution("01033334444"))
