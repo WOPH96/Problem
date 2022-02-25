767dcf8ce63a76c4236117389e1c402fbb74d63f
양원필 <yang-wonphil@Phils-MacBookPro.local>
2022-02-25 17:39:06 +0900

    move

--- cpp/cmake_used/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/yang-wonphil/Desktop/study/programmers/cpp/cmake_used
-# Build directory: /Users/yang-wonphil/Desktop/study/programmers/cpp/cmake_used/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- cpp/cmake_used/date_without_constructor/README.md
+++
@@ -1,5 +0,0 @@
-# Date 클래스 디자인
-
-## 변수 -> year,month,date
-
-## 함수 -> setdate,addday,..
