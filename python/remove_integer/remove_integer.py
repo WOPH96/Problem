0ac3b760c3e3774a468d059280488ab1acf3637c
양원필 <yang-wonphil@Phils-MacBookPro.local>
2022-02-28 19:33:21 +0900

    re_home

--- python/remove_integer/remove_integer.py
+++ python/remove_integer/remove_integer.py
@@ -1,6 +1,13 @@
 def solution(arr):
     answer = []
+    if len(arr) == 1:
+        answer.append(-1)
+    else:
+        #sorted(arr, reverse=1)
+        # arr.pop()
+        # answer.extend(arr)
     return answer
 
-print(solution([4,3,2,1]))
+
+print(solution([4, 3, 2, 1]))
 print(solution([10]))
