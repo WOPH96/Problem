cd7450e63dab75954b53fc2f56ec615185a2341c
Philip Yang <ywonphil@gmail.com>
2022-03-01 10:28:04 +0900

    .

---
+++ python/weird_str/weird_str.py
@@ -0,0 +1,12 @@
+def solution(s):
+    answer = ''
+    str = s.split(" ")
+    print(str)
+    for word in str:
+        for i in range(len(word)):
+            if i % 2 == 0:
+                word[i]
+    return answer
+
+
+print(solution("try hello world"))
