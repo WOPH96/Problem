3720dfa11cffca5eb516bd1a740d2a6c9a93a01e
Philip Yang <ywonphil@gmail.com>
2022-03-02 22:40:51 +0900

    .

---
+++ cpp/main.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+// 소수 = 이전의 소수로 안나눠떨어지면 소수?
+
+int solution(int n)
+{
+    vector<int> prime;
+    prime.push_back(2);
+
+    for (int i = 2; i <= n; i++)
+    {
+        while ()
+    }
+
+    return prime.size();
+}
+
+int main()
+{
+
+    cout << solution(10) << endl;
+
+    return 0;
+}
