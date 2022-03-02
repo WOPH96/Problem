1282d388990fe1cbd1d360d03d6b478f928198c7
양원필 <yang-wonphil@Phils-MacBookPro.local>
2022-03-02 19:57:37 +0900

    22_03_02_re

--- python/prime_num/prime_num.py
+++ python/prime_num/prime_num.py
@@ -1,12 +1,15 @@
-def solution(n):
-    answer = 0
-    i = 1
-    prime = 1
-    for num in n:
-        n % num == 0:
-            break
-
-    return answer
+# def solution(n):
+#     i = 0
+#     prime = 0
+#     for num in range(2, n+1):
+#         for q in range(2, num+1):
+#             if num % q == 0:
+#                 i += 1
+#         if i == 1:
+#             prime += 1
+#         i = 0
+#     return prime
 
 
 print(solution(10))
+print(solution(5))
