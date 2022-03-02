b55d648a2e3288a58c64c554d8021ec22827bc5d
Philip Yang <ywonphil@gmail.com>
2022-03-02 21:52:40 +0900

    .

--- python/prime_num/prime_num.py
+++ python/prime_num/prime_num.py
@@ -10,6 +10,22 @@
 #         i = 0
 #     return prime
 
+# 소수 = 이전의 소수로 안나눠떨어지면 소수?
+
+def solution(n):
+    prime = [2]
+
+    for i in range(2, n+1):
+        for num in prime:
+            if i % num != 0:
+                break
+        if num == prime[-1]:
+            prime.append(i)
+            print(prime)
+
+    return len(prime)
+
 
 print(solution(10))
-print(solution(5))
+# print(solution(5))
+# print(solution(1000000))
