8b3d92197e50c68dec135f57c4782f3de6ab5590
양원필 <yang-wonphil@Phils-MacBookPro.local>
2022-03-02 19:23:21 +0900

    22_03_02

--- python/weird_str/weird_str.py
+++ python/weird_str/weird_str.py
@@ -1,12 +1,31 @@
+# def solutions(s):
+#     answer = ''
+#     str = s.split(" ")
+#     for word in str:
+#         for i in range(len(word)):
+#             if i % 2 == 0:
+#                 answer += word[i].upper()
+#             else:
+#                 answer += word[i]
+#         answer += " "
+#     return answer
+
+
 def solution(s):
     answer = ''
-    str = s.split(" ")
-    print(str)
-    for word in str:
-        for i in range(len(word)):
-            if i % 2 == 0:
-                word[i] = word[i].upper()
+    i = 0
+    for word in s:
+        if word == ' ':
+            i = 0
+            answer += word
+        elif i % 2 == 0:
+            answer += word.upper()
+            i += 1
+        elif i % 2 == 1:
+            answer += word.lower()
+            i += 1
+
     return answer
 
 
-print(solution("try hello world"))
+print(solution("try      hello world"))
