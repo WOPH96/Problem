b029c70069a1efb8a76341eba9c643332ef0f7db
양원필 <yang-wonphil@Phils-MacBookPro.local>
2022-03-11 20:03:30 +0900

    re

---
+++ python/min_rectangle.py
@@ -0,0 +1,15 @@
+
+def solution(sizes):
+    answer = 0
+
+    w = sorted(sizes, key=lambda x: x[0], reverse=True)
+    h = sorted(sizes, key=lambda x: x[1], reverse=True)
+
+    if(w[0][0])
+
+    print(w)
+    print(h)
+    return answer
+
+
+print(solution([[60, 50], [30, 70], [60, 30], [80, 40]]))
