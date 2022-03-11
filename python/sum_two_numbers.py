0ce58fd2331ec9d4ad47eab84d6fa58dd08632c1
Wonphil Yang <ywonphil@gmail.com>
2022-03-12 00:42:08 +0900

    22_03_12

--- python/sum_two_numbers.py
+++ python/sum_two_numbers.py
@@ -1,1 +1,11 @@
-집가서 해보기@@
+from itertools import combinations
+
+
+def solution(numbers):
+    
+    sums = set()
+    for i in combinations(numbers,2):
+        sums.add(i[0]+i[1])
+    return sorted(list(sums))
+
+print(solution([2,1,3,4,1]))
