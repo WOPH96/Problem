656b1a11ee7d4169eb295128bf17b2827711a508
Philip Yang <ywonphil@gmail.com>
2022-03-20 21:55:49 +0900

    22_03_20_re

--- python/lv2/tuple.py
+++ python/lv2/tuple.py
@@ -1,10 +1,40 @@
 
-def solution(s):
+def tointlist(s):
     answer = []
-    st = set()
+    flag = False
 
-    print(s)
+    i = 1
+    num = -1
+    # print(len(s)-1)
+    while(i < len(s)-1):
+        if not flag:
+            if s[i] == ",":
+                i += 1
+                continue
+            answer.append([])
+            num += 1
+        if s[i] == "{":
+            flag = True
+            i += 1
+            continue
+        elif s[i] == ",":
+            i += 1
+            continue
+        elif s[i] == "}":
+            flag = False
+
+        if flag:
+            answer[num].append(int(s[i]))
+        i += 1
     return answer
+
+
+def solution(s):
+    st = set()
+    s = tointlist(s)
+    print(s)
+
+    return st
 
 
 print(solution("{{2},{2,1},{2,1,3},{2,1,3,4}}"))
