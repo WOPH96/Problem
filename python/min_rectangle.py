9f8fac6ae680e891b4c7d8bd3afa499cc89a646b
Philip Yang <ywonphil@gmail.com>
2022-03-20 12:37:48 +0900

    22_03_20

--- python/min_rectangle.py
+++ python/min_rectangle.py
@@ -1,15 +1,18 @@
-
 def solution(sizes):
     answer = 0
+    trans = list(map(list, zip(*sizes)))
+    print(trans)
 
-    w = sorted(sizes, key=lambda x: x[0], reverse=True)
-    h = sorted(sizes, key=lambda x: x[1], reverse=True)
+    mx = [max(trans[0]), max(trans[1])]
+    M = mx.index(max(mx))
+    m = m = M ^ 1
 
-    if(w[0][0])
+    while True:
+        idx = trans[m].index(mx[m])  # 작은놈의 맥스 위치
+        Maxidx = trans[M].index(mx[M])
+        if trans[m][idx] <= trans[M][idx]:  # 짝보다 작으면
+            break
+        trans[m][idx], trans[M][idx] = trans[M][idx], trans[m][idx]  # 짝보다 크면 스왑
+        mx[m] = max(trans[m])
 
-    print(w)
-    print(h)
-    return answer
-
-
-print(solution([[60, 50], [30, 70], [60, 30], [80, 40]]))
+    return mx[0]*mx[1]
