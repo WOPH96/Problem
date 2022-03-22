73e9c5b738d83372957fc36d7e64224a24615a17
Wonphil Yang <ywonphil@gmail.com>
2022-03-23 02:16:03 +0900

    22_03_23_re

--- teer/virus.py
+++ teer/virus.py
@@ -2,20 +2,23 @@
 import sys
 import math
 
-k, p, n = map(int, input().split())
-
+#k, p, n = map(int, input().split())
+k, p, n = (100000000, 100000000, 1000000)
 mod = 1000000007
 
-x = math.ceil(math.log(p, mod))+1  # p^x > mod
+x = math.ceil(math.log(p, mod))  # p^x > mod
+print("first =",x)
 for i in range(x, n+1):
     if n % i == 0:
         x = i
         break
-
+print("second =",x)
 
 # k * p^n % mod
 # k = k* (p**n) % mod
 # k = (p^x%mod)^(n/x) %mod
+
+# print(int(p**x)%mod)
 
 print(
 
