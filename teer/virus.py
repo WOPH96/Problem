2ef5022adcba996e74dccdf08d140aff15ee91c5
Wonphil Yang <ywonphil@gmail.com>
2022-03-23 02:59:44 +0900

    22_03_23

--- teer/virus.py
+++ teer/virus.py
@@ -6,24 +6,25 @@ k, p, n = map(int,sys.stdin.readline().split())
 #k, p, n = (100000000, 100000000, 1000000)
 mod = 1000000007
 
-x = math.ceil(math.log(p, mod))  # p^x > mod
-print("first =",x)
-for i in range(x, n+1):
-    if n % i == 0:
-        x = i
-        break
-print("second =",x)
 
-# k * p^n % mod
+
+
 # k = k* (p**n) % mod
-# k = (p^x%mod)^(n/x) %mod
 
-# print(int(p**x)%mod)
+result = 1
 
-print(
+#k*(p**n) %mod =  p * p * p * p * p ... * p * k  % mod
 
-    (k % mod * ((int(p**x) % mod) ** int((n/x))) % mod) % mod
-)
+for _ in range(n):
+    result*=p
+    result%=mod
+
+result*=k
+result%=mod
+
+print(result)
+
+
 
 
 # p**n % 1000000007 = 큰값
