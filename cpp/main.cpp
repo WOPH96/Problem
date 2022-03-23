ad90918671e54144a757e1dd01367f5827af3258
Wonphil Yang <ywonphil@gmail.com>
2022-03-24 00:30:59 +0900

    22_03_24_같은수더하기

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,11 +1,18 @@
 #include <string>
 #include <vector>
-
+#include <iostream>
 using namespace std;
 
 vector<long long> solution(int x, int n)
 {
     vector<long long> answer;
+    tmp = x;
+    while (n)
+    {
+        answer.push_back(x);
+        x += tmp;
+        n -= 1;
+    }
 
     return answer;
 }
