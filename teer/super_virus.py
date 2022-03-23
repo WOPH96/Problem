648a8100ca0927863996eb029552989d11e55134
Wonphil Yang <ywonphil@gmail.com>
2022-03-23 23:23:11 +0900

    22_03_23

--- teer/super_virus.py
+++ teer/super_virus.py
@@ -14,7 +14,36 @@ result = 1
 # k*(p**n) %mod =  p * p * p * p * p ... * p * k  % mod
 
 for _ in range(n):
-    result *= p
+    result *= pimport sys
+
+k,p,n = map(int,sys.stdin.readline().split())
+
+# 0.1초당 P배씩 증가
+# 1초당 P**10씩 증가
+mod = 1000000007
+res = 1
+
+#res = k * (p **10)**n % mod
+
+
+# increase per sec
+ips = 1
+
+for _ in range(10):
+    ips*=p
+    ips%=mod
+
+# main
+
+for _ in range(n):
+    res*=ips
+    res%=mod
+
+res*=k
+res%=mod
+
+print(res)
+
     result %= mod
 
 result *= k
@@ -24,3 +53,35 @@ print(result)
 
 
 # p**n % 1000000007 = 큰값
+ '''
+ import sys
+
+k,p,n = map(int,sys.stdin.readline().split())
+
+# 0.1초당 P배씩 증가
+# 1초당 P**10씩 증가
+mod = 1000000007
+res = 1
+
+#res = k * (p **10)**n % mod
+
+
+# increase per sec
+ips = 1
+
+for _ in range(10):
+    ips*=p
+    ips%=mod
+
+# main
+
+for _ in range(n):
+    res*=ips
+    res%=mod
+
+res*=k
+res%=mod
+
+print(res)
+
+ '''
