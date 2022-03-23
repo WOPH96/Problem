ffbd51c92b40b2449813cbfecfc0cbe27c021ad9
Wonphil Yang <ywonphil@gmail.com>
2022-03-24 00:47:06 +0900

    22_03_24

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,24 +1,23 @@
 #include <string>
 #include <vector>
 #include <iostream>
+
 using namespace std;
 
-vector<long long> solution(int x, int n)
+vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2)
 {
-    vector<long long> answer;
-    tmp = x;
-    while (n)
-    {
-        answer.push_back(x);
-        x += tmp;
-        n -= 1;
-    }
+    vector<vector<int>> answer;
+
+    auto a = arr1.size();
+    auto b = arr1[0].size();
+    auto c = arr1[1].size();
+
+    cout << a << b << c;
 
     return answer;
 }
 
 int main()
 {
-
     return 0;
 }
