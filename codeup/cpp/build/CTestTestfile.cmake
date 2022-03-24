4fe7e6e6cc016a41d5133a841bc27d3c2c76b927
Wonphil Yang <ywonphil@gmail.com>
2022-03-24 22:23:07 +0900

    .

---
+++ codeup/cpp/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/c/Users/ywonp/Documents/study/programmers/codeup/cpp
+# Build directory: /mnt/c/Users/ywonp/Documents/study/programmers/codeup/cpp/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
