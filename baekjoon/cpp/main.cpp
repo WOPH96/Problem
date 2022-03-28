0618e60a828b2e3e358f83d3131fd8355c3868e8
Philip Yang <ywonphil@gmail.com>
2022-03-28 22:08:49 +0900

    보물

--- baekjoon/cpp/main.cpp
+++ baekjoon/cpp/main.cpp
@@ -3,52 +3,89 @@
 // #include <iostream>
 using namespace std;
 /*
-55-50+40
-=-35
-
--하나라도 나오면 전부 그 뒤는 빼버린다.
+5
+1 1 1 6 0
+2 7 8 3 1
  */
-
 int main()
 {
     ios_base ::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    string str;
-    cin >> str;
+    int T;
 
-    bool isMinus = false;
+    cin >> T;
 
-    int res = 0;
-    string num = "";
+    vector<int> A;
+    vector<int> B;
 
-    for (const char &elem : str)
+    A.reserve(T);
+    B.reserve(T);
+    for (int j = 0; j < T; j++)
     {
-        if (elem == '-' || elem == '+' || elem == *str.end())
-        {
-            if (isMinus)
-            {
-                res -= stoi(num);
-            }
-            else
-            {
-                res += stoi(num);
-            }
-            num = "";
-        }
-        else
-        {
-            num += elem;
-        }
-
-        if (elem == '-')
-            isMinus = true;
+        int in;
+        cin >> in;
+        A.push_back(in);
     }
-    if (isMinus)
-        res -= stoi(num);
-    else
-        res += stoi(num);
-    cout << res << endl;
+
+    for (int j = 0; j < T; j++)
+    {
+        int in;
+        cin >> in;
+        B.push_back(in);
+    }
+
+    vector<pair<int, int>> C;
+    for (int i = 0; i < T; i++)
+    {
+        C.push_back({B[i], i});
+    }
+
+    sort(C.begin(), C.end());
+
+    // for (const auto &elem : C)
+    // {
+    //     cout << elem.first << elem.second << " ";
+    // }
+    // cout << endl;
+
+    sort(A.begin(), A.end(), greater<int>());
+    // for (const auto &elem : A)
+    // {
+    //     cout << elem << " ";
+    // }
+    // cout << endl;
+
+    vector<pair<int, int>> result;
+    result.reserve(T);
+
+    for (int i = 0; i < T; i++)
+    {
+        result.push_back({C[i].second, A[i]});
+    }
+
+    sort(result.begin(), result.end());
+
+    // for (const auto &elem : result)
+    // {
+    //     cout << elem.first << elem.second << " ";
+    // }
+
+    for (int i = 0; i < T; i++)
+    {
+        A[i] = result[i].second;
+    }
+
+    // for (const auto &elem : A)
+    // {
+    //     cout << elem << " ";
+    // }
+    int sum = 0;
+    for (int i = 0; i < T; i++)
+    {
+        sum += A[i] * B[i];
+    }
+    cout << sum << endl;
     return 0;
 }
