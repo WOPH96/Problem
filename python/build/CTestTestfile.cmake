f57a9a8e762fcf3df40fc8ef86b7d132c8be2f67
양원필 <yang-wonphil@Phils-MacBookPro.local>
2022-03-29 15:42:50 +0900

    .

--- python/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/yang-wonphil/Desktop/study/programmers/python
-# Build directory: /Users/yang-wonphil/Desktop/study/programmers/python/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
