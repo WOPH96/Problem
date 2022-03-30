19bf82cf8f83a3a4bdf5bd22128fb038ff10b2af
양원필 <yang-wonphil@Phils-MacBookPro.local>
2022-03-30 17:03:26 +0900

    jw_re

--- baekjoon/cpp/main.cpp
+++ baekjoon/cpp/main.cpp
@@ -3,54 +3,118 @@
 // #include <iostream>
 using namespace std;
 /*
-첫째 줄에 A, B (1 ≤ A < B ≤ 1e9)가 주어진다.
+첫째 줄에 N과 K가 주어진다. (1 ≤ N, K ≤ 300,000)
 
-2 162 => 5
-4 42 => -1
-100 40021 => 5
+다음 N개 줄에는 각 보석의 정보 Mi와 Vi가 주어진다. (0 ≤ Mi, Vi ≤ 1,000,000)
+
+다음 K개 줄에는 가방에 담을 수 있는 최대 무게 Ci가 주어진다. (1 ≤ Ci ≤ 100,000,000)
+
+모든 숫자는 양의 정수이다.
+
 */
 
 //#define MAX 10
 
-int init, finish;
+int N, K;
+vector<pair<int, int>> jw;
+vector<int> bag;
+
+bool comp_value(const pair<int, int> &a, const pair<int, int> &b)
+{
+    if (a.second == b.second)
+        return a.first < b.first;
+    return a.second < b.second;
+}
+template <typename T>
+void print(const vector<pair<T, T>> &v)
+{
+    for (const auto &elem : v)
+    {
+        cout << "(" << elem.first << "," << elem.second << ")"
+             << " ";
+    }
+    cout << endl;
+}
+
+template <typename T>
+void print(const vector<T> &v)
+{
+    for (const auto &elem : v)
+    {
+        cout << elem << " ";
+    }
+    cout << endl;
+}
 
 void input()
 {
     // ifstream ifs;
     // ifs.open("input.txt");
-    // ifs >> init >> finish;
+    // ifs >> N >> K;
+    // jw.reserve(N);
+    // bag.reserve(K);
+    // for (int i = 0; i < N; i++)
+    // {
+    //     int M, V;
+    //     ifs >> M >> V;
+    //     jw.push_back({M, V});
+    // }
+    // for (int i = 0; i < K; i++)
+    // {
+    //     int C;
+    //     ifs >> C;
+    //     bag.push_back(C);
+    // }
 
-    // cout << init << " " << finish << endl;
-    cin >> init >> finish;
+    cin >> N >> K;
+    jw.reserve(N);
+    bag.reserve(K);
+    for (int i = 0; i < N; i++)
+    {
+        int M, V;
+        cin >> M >> V;
+        jw.push_back({M, V});
+    }
+    for (int i = 0; i < K; i++)
+    {
+        int C;
+        cin >> C;
+        bag.push_back(C);
+    }
+
+    // print(jw);
+    // print(bag);
 }
 
 void solution()
 {
-    int cnt = 1;
-    while (finish > init)
+    sort(jw.begin(), jw.end(), comp_value);
+    // sort(jw.begin(), jw.end(), [](const pair<int, int> &a, const pair<int, int> &b) -> bool
+    //      { return a.second < b.second; });
+    // for (const auto &elem : jw)
+    // {
+    //     cout << "(" << elem.first << "," << elem.second << ")"
+    //          << " ";
+    // }
+    sort(bag.begin(), bag.end());
+    // print(jw);
+    // print(bag);
+
+    long long val = 0;
+    while (!bag.empty() && !jw.empty())
     {
-        if (finish % 10 == 1 || finish % 2 == 0)
-        {
-            if (finish % 10 == 1)
-                finish /= 10;
-            else
-                finish /= 2;
-            cnt++;
-        }
+        if (bag.back() < jw.back().first)
+            jw.pop_back();
         else
         {
-            cout << -1 << endl;
-            return;
+            auto itr = lower_bound(bag.begin(), bag.end(), jw.back().first);
+            bag.erase(itr);
+            val += jw.back().second;
+            // print(bag);
+            jw.pop_back();
         }
     }
-    if (finish == init)
-    {
-        cout << cnt << endl;
-    }
-    else
-    {
-        cout << -1 << endl;
-    }
+    cout << val << endl;
 }
 
 void solve()
--- baekjoon/cpp/tempCodeRunnerFile.cpp
+++ baekjoon/cpp/tempCodeRunnerFile.cpp
@@ -1,1 +1,2 @@
-long
+int(jw);
+    // pr
