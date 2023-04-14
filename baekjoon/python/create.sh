4c5a0f1b6c76505d2986e075f742136f02988586
양원필 <yang-wonphil@Phils-MacBookPro.local>
2023-04-14 09:36:57 +0900

    create_open_method

--- baekjoon/python/create.sh
+++ baekjoon/python/create.sh
@@ -5,11 +5,13 @@ if [ -z $1 ] ; then
     exit 0
 fi
 
+#crete
 mkdir $1
 touch $1/$1.py
 touch $1/$1_input.txt
 echo "import sys" >> $1/$1.py
 echo "sys.stdin = open('$1_input.txt','r')" >> $1/$1.py
 
+#open
 code $1/$1.py
 code $1/$1_input.txt
