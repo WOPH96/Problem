63796f7f6ba958b6135ab22826d21f27de71b1ad
wonphil <ywonphil@gmail.com>
2023-04-14 20:42:39 +0900

    2668

--- baekjoon/python/2667/2667.py
+++ baekjoon/python/2667/2667.py
@@ -16,7 +16,8 @@ def PR():
 
 def dfs(x, y):
     s = []
-    s.append(x, y)
+    s.append((x, y))
+    house = 0
     while s:
         # 현 위치
         x, y = s.pop()
@@ -25,22 +26,36 @@ def dfs(x, y):
             continue
         # 집 있으면
         if graph[x][y] == 1:
-            # 집 없음 처리하고 주변에 있는 집 샅샅이 뒤지기
+            # 집 카운트, 주변에 있는 집 샅샅이 뒤지기
+            house += 1
+            graph[x][y] = 0
+
             for i in range(4):
                 nx = x + dx[i]
                 ny = y + dy[i]
-                if()
+                # 벽이면
+                if nx < 0 or nx >= n or ny < 0 or ny >= n:
+                    continue
+                if graph[nx][ny] == 0:
+                    continue
+                if graph[nx][ny] == 1:
+                    s.append((nx, ny))
+    return house
+
 
 def sol():
     housing = []
     danzi = 0
-    for i in range(N):
-        for j in range(N):
+    for i in range(n):
+        for j in range(n):
             house = dfs(i, j)
             if house != 0:
                 housing.append(house)
                 danzi += 1
+    print(danzi)
+    housing.sort()
+    for h in housing:
+        print(h)
 
 
 sol()
-PR()
