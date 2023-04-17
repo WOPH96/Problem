329dcfe17352a9f0128b541375c0b75db1b808a3
wonphil <ywonphil@gmail.com>
2023-04-17 13:28:04 +0900

    tomato2

--- baekjoon/python/7569/7569.py
+++ baekjoon/python/7569/7569.py
@@ -1,11 +1,14 @@
 from collections import deque
 import sys
-sys.stdin = open('7579_input.txt', 'r')
+sys.stdin = open('7569_input.txt', 'r')
 
 
-m, n = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(n)]
-
+m, n, h = map(int, input().split())
+graph = []
+for _ in range(h):
+    # z축이 맨 앞
+    temp = [list(map(int, input().split())) for _ in range(n)]
+    graph.append(temp)
 dx = [0, 0, 1, -1, 0, 0]
 dy = [1, -1, 0, 0, 0, 0]
 dz = [0, 0, 0, 0, 1, -1]
@@ -13,7 +16,9 @@ dz = [0, 0, 0, 0, 1, -1]
 
 def gr(graph):
     for i in graph:
-        print(i)
+        for j in i:
+            print(j)
+        print()
     print()
 
 
@@ -22,38 +27,47 @@ def bfs(start):
     q = deque()
     for riped in start:
         q.append(riped)
+    # print(q/)
     while q:
         # gr(graph)
-        x, y = q.popleft()
-        for i in range(4):
+        z, x, y = q.popleft()
+        for i in range(6):
             nx = x + dx[i]
             ny = y + dy[i]
-            # 벽이면
-            if nx < 0 or nx >= n or ny < 0 or ny >= m:
-                continue
-            # 안익은 토마토일때만 / 벽 일때, 익은 토마토일때 X
-            if graph[nx][ny] == 0:
-                # 익는 날짜 추가
-                graph[nx][ny] = graph[x][y] + 1
-                # 내일 익는 토마토 큐에 추가
-                q.append((nx, ny))
-
-
-fail = False
+            nz = z + dz[i]
+            # 벽이 아닐때만 진행
+            if 0 <= nz < h and 0 <= nx < n and 0 <= ny < m:
+                # 안익은 토마토일때
+                if graph[nz][nx][ny] == 0:
+                    # 익는 날짜 추가
+                    graph[nz][nx][ny] = graph[z][x][y] + 1
+                    # 내일 익는 토마토 큐에 추가
+                    q.append((nz, nx, ny))
 
 
 # 익은 토마토 위치 확인
 pos = []
-for i in range(n):
-    for j in range(m):
-        if graph[i][j] == 1:
-            pos.append((i, j))
+for k in range(h):
+    for i in range(n):
+        for j in range(m):
+            if graph[k][i][j] == 1:
+                pos.append((k, i, j))
+# print(pos)
 bfs(pos)
 
+fail = False
+
+# gr(graph)
+
+MAX = []
 for lst in graph:
-    if 0 in lst:
-        fail = True
+    for elem in lst:
+        if 0 in elem:
+            fail = True
+            break
+    MAX.append(max(map(max, lst)))
+
 if fail:
     print(-1)
 else:
-    print(max(map(max, graph))-1)
+    print(max(MAX)-1)
