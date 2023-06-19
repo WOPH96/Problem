56daf4217d907b6075e41905668947947e2cf8de
양원필 <yang-wonphil@Phils-MacBookPro.local>
2023-06-19 17:23:39 +0900

    .

---
+++ baekjoon/python/1339/1339.py
@@ -0,0 +1,32 @@
+import sys
+sys.stdin = open('1339_input.txt','r')
+
+n = int(input())
+str = [input() for _ in range(n)]
+
+print(str)
+
+'''
+높은 자리 수 부터 높은 수 할당
+차이만큼 올려 = gap
+GCF 
+012 ->
+234
+ACDEB
+01234
+
+알파벳 숫자 할당은 어떻게?
+dict?
+
+dic = dict()
+
+num = 9
+if not dic[해당 알파벳]:  # none이면 
+    dic[해당 알파벳] = num
+
+'''
+a = None
+if not a :
+    print ("hello")
+
+
