661a59f9f394e212d1b0261a5175ad827b4faac3
양원필 <yang-wonphil@Phils-MacBookPro.local>
2023-06-19 15:19:06 +0900

    .

--- baekjoon/python/1202/1202.py
+++ baekjoon/python/1202/1202.py
@@ -1,14 +1,15 @@
 import sys
+import heapq as hq
 sys.stdin = open('1202_input.txt','r')
 
 n,k = map(int,sys.stdin.readline().split())
 
 #보석 m,v 
 jws = []
-visited = [False]*n
+
 for _ in range(n):
-    m,v = map(int,sys.stdin.readline().split())
-    jws.append([m,v])
+    weight,value = map(int,sys.stdin.readline().split())
+    jws.append([weight,value])
 
 bags = []
 for _ in range(k):
@@ -16,31 +17,54 @@ for _ in range(k):
     bags.append(c)
 
 # print(jws,bags,visited,sep="\n")
+jws.sort()
+bags.sort() # 작은 가방 순서로 정렬
 
-jws.sort(key=lambda x:-x[1])
-bags.sort()
+    
 # print(jws,bags,sep="\n")
+'''
+모든 가방을 돌면서 해당 무게보다 가벼운 보석들 넣기
+가방보다 무거운 보석 나왔을 때 멈추고
+그중에 가장 비싼 보석 저장
 
-sum = 0 
+다음 보석부터 다시 돌아
+
+'''
+sum=0
+idx=0
+heap = []
 for bag in bags:
-    for i in range(n):
-        if bag >= jws[i][0] and not visited[i] :
-            sum +=jws[i][1]
-            visited[i] = True
-            break
-print(sum)
+    while(idx<n and bag>=jws[idx][0]):
+        hq.heappush(heap,[-jws[idx][1],jws[idx][1]])
+        idx+=1
+    if(heap):
+        sum+=hq.heappop(heap)[1]    
+
+print(sum)        
+
+
+    
 
 '''
 
 제일 비싼 가격을 받아야 하니까
 가격순 정렬
-2,99
-1,65
-5,23
+11,100  = 못먹는거
+3,99    = 젤작은걸론 못먹
+10,65   = 먹을 수 있음
+2,23    = 먹을수 있음 
+
+
+못담아가면 버리는거지
+그럼 제일 비싼거 뽑아서
+작은 가방이 담을수있어야
+
 
 작은 가방이 먹을 수 있는 거 위에서부터 고르기
 2 -> 2,99
 10 -> 1,65
 99+65 = 164
+
+
 
 ''' 
