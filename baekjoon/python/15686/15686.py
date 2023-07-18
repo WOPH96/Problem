8367fa56c576dd3a39e7223363a0d61720b6519c
양원필 <yang-wonphil@Phils-MacBookPro.local>
2023-07-18 17:16:33 +0900

    re

---
+++ baekjoon/python/15686/15686.py
@@ -0,0 +1,9 @@
+import sys
+sys.stdin = open('15686_input.txt', 'r')
+
+n, m = map(int, input().split())
+
+city = [list(map(int, input().split())) for _ in range(n)]
+
+for elem in city:
+    print(elem)
