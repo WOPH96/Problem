b6cf42f1db2450932ec42b8614fee0e82279a216
양원필 <yang-wonphil@Phils-MacBookPro.local>
2023-07-20 17:07:14 +0900

    no_msg

---
+++ baekjoon/python/15683/15683.py
@@ -0,0 +1,22 @@
+import sys
+sys.stdin = open('15683_input.txt','r')
+
+'''
+dfs -> 탐색
+cctv위치 
+cctv종류에 따른 mode
+direction
+
+'''
+
+cctv=[]
+
+
+mode=[
+[],
+[[0],[1],[2],[3]],
+[],
+[],
+[],
+[],
+]
