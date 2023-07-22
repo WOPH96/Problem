570f57578b79d2ece18d3032d13150a70bdc8259
wonphil <ywonphil@gmail.com>
2023-07-22 16:32:06 +0900

    re

--- baekjoon/python/2437/2437.py
+++ baekjoon/python/2437/2437.py
@@ -8,27 +8,37 @@ weights.sort()
 # print(weights)
 
 '''
-0 ~ n 합
-0+1 ~ n 까지 합
-0+1+2 ~ n 까지 합
+idea
 
-set
+target수가 나오기 전까진 모두 만들 수 있음
+
+1 1 2 3 6 7 30
+
+1 < 1
+2+1 < 1
+3+1 < 2
+4+2 < 3
+6+3 < 6
+9+6 < 7
+15+7 <30
+22 !  
+
+
 '''
 
-sums = set()
+print(weights)
+# for i in range(n):
+#     sums.add(weights[i]) # 추 하나만
+#     for j in range(i+1,n):
+#         print(weights[i:j+1])
+#         sums.add(sum(weights[i:j+1]))
 
-for i in range(n):
-    sums.add(weights[i]) # 추 하나만
-    for j in range(i+1,n):
-        print(weights[:i+1],weights[j])
-        sums.add(sum(weights[:i+1])+weights[j])
+# print(weights,sums)
 
-print(weights,sums)
-
-for i in range(1,100000000000):
-    if i not in sums:
-        print(i)
-        break
+# for i in range(1,100000000000):
+#     if i not in sums:
+#         print(i)
+#         break
 
 # from itertools import combinations
 # sums=set()
