c92b326787c3d92a4d7248a975a92c6591998bd8
양원필 <yang-wonphil@Phils-MacBookPro.local>
2023-07-25 13:51:53 +0900

    no_msg

--- programmers/python/43612/43612.py
+++ programmers/python/43612/43612.py
@@ -3,7 +3,7 @@ sys.stdin = open('43612_input.txt','r')
 
 def solution(n, computers):
     answer = 0
-    print(n,computers)
+    # print(n,computers)
 
     from collections import deque
     visited=[False]*n
@@ -13,22 +13,20 @@ def solution(n, computers):
         while q:
             out = q.popleft()
             if not visited[out] :
+                
                 visited[out] = True
                 for nv,connect in enumerate(G[out]):
-                    # print(G[out])
-                    if nv == out:
-                        continue
-                    else:
-                        if connect == 1:
-                            q.append(nv)
-                            visited[nv] = True
-
+                    if nv != out and connect==1:
+                        # print(out,nv,connect)
+                        q.append(nv)
+    
     for i in range(n):
         if not visited[i]:
             bfs(computers,i)
+            # print()
             answer+=1
 
     return answer
 
 
-print(solution(3, [[1, 1, 0], [1, 1, 1], [0, 1, 1]]))
+print(solution(3, [[1, 1, 0], [1, 1, 0], [0, 0, 1]]))
