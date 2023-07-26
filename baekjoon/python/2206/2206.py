37fd816eacab991fa7fb729828e4e9a14b2f39f7
양원필 <yang-wonphil@Phils-MacBookPro.local>
2023-07-26 17:14:21 +0900

    no_msg

--- baekjoon/python/2206/2206.py
+++ baekjoon/python/2206/2206.py
@@ -1,2 +1,10 @@
 import sys
-sys.stdin = open('2206_input.txt','r')
+sys.stdin = open('2206_input.txt', 'r')
+
+n, m = map(int, input().split())
+graph = []
+
+for i in range(n):
+    graph.append(list(map(int, input())))
+
+print(graph)
