197fbb06ddbb1d8c3fe45a9e8f9ef9d419a8999b
양원필 <yang-wonphil@Phils-MacBookPro.local>
2023-08-07 15:42:30 +0900

    no_msg

---
+++ programmers/python/181188/181188.py
@@ -0,0 +1,18 @@
+import sys
+
+
+def solution(targets):
+    answer = 0
+    targets.sort(reverse=True)
+    # print(targets)
+    s = []
+    s.append(targets.pop())
+    while targets:
+        start, end = targets.pop()
+        if start >= s[-1][1]:
+            s.append([start, end])
+    print(len(s))
+    return answer
+
+
+solution([[4, 5], [4, 8], [10, 14], [11, 13], [5, 12], [3, 7], [1, 4]])
