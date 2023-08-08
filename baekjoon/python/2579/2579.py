9287b4831c975ad3a43abc5c1665bb20a35caacd
양원필 <yang-wonphil@Phils-MacBookPro.local>
2023-08-08 16:53:56 +0900

    no_msg

--- baekjoon/python/2579/2579.py
+++ baekjoon/python/2579/2579.py
@@ -3,16 +3,18 @@ sys.stdin = open('2579_input.txt', 'r')
 
 n = int(input())
 
-l = [int(input()) for _ in range(n)]
+steps = [int(input()) for _ in range(n)]
+
+print(steps)
+
+dp = [0]*n
+dp[n-1] = steps[n-1]
+print(dp)
 
 '''
-최댓값 출력
-i,i+1중에서 최댓값 저장
-세개 연속 밟으면 안됨
-마지막은 무조건 밟아야함
-마지막에서 시작하면 되겠다
+dp[i]는 그 계단을 밟았을 때 최대의 수
+마지막 계단은 무조건 밟아야 하니까.. 거꾸로 진행하는게 나을듯함
 
-i, i+1, i+2 
 
 
 
