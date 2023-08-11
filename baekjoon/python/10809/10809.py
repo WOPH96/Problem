26dae3cdddc7c645856b2a7f77929cec0ae760a7
양원필 <yang-wonphil@Phils-MacBookPro.local>
2023-08-11 09:42:53 +0900

    no_msg

--- baekjoon/python/10809/10809.py
+++ baekjoon/python/10809/10809.py
@@ -1,7 +1,13 @@
 import sys
-sys.stdin = open('10809_input.txt','r')
+sys.stdin = open('10809_input.txt', 'r')
 
 string = input()
 
-for st in range(26):
-    
+for i in range(26):
+    ch = chr(i+97)
+    print(string.find(ch), end=" ")
+    # if ch in string:
+    #     print(string.index(ch), end=" ")
+    # else:
+    #     print(-1, end=" ")
+print()
