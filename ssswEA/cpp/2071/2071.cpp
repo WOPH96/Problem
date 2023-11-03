e6232cc8e89144fd1c043df9f340af555b58c415
WOPH96 <ywonphil@gmail.com>
2023-11-04 00:01:38 +0900

    no_msg

---
+++ ssswEA/cpp/2071/2071.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    freopen("2071_input.txt", "r", stdin);
+
+
+    return 0;
+}
