278214e60a2333d14d227edcfe15a21494758ced
양원필 <yang-wonphil@Phils-MacBookPro.local>
2023-11-03 15:37:16 +0900

    no_msg

---
+++ baekjoon/cpp/18108/18108.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    freopen("18108_input.txt", "r", stdin);
+
+        return 0;
+}
