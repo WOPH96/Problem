f5f367bbb00fb4c10e37d0012c9fa30906a2ca9c
WOPH96 <ywonphil@gmail.com>
2023-11-04 11:57:53 +0900

    no_msg

--- ssswEA/cpp/2071/2071.cpp
+++ ssswEA/cpp/2071/2071.cpp
@@ -8,6 +8,19 @@ int main()
     cin.tie(NULL);
     freopen("2071_input.txt", "r", stdin);
 
+    int t ;
+    cin >> t;
+    for(int i=1;i<=t;i++){
+        int sum = 0;
+        for(int j=0;j<10;j++){
+            int temp;
+            cin >> temp;
+            if(temp%2==1){
+                sum+=temp;
+            }
+        }
+        cout << "#"<<i <<" "<<sum<<"\n";
+    }
 
     return 0;
 }
