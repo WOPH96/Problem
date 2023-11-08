16872590dc7e67385a86f34e6f6e3a4374e6cce5
양원필 <yang-wonphil@Phils-MacBookPro.local>
2023-11-08 17:25:19 +0900

    no_msg

---
+++ programmers/cpp/154539/154539.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+vector<int> solution(vector<int> numbers)
+{
+    vector<int> answer;
+
+    // 뒷 큰 수
+
+    return answer;
+}
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    freopen("154539_input.txt", "r", stdin);
+
+    solution({2, 3, 3, 5});       //	[3, 5, 5, -1]
+    solution({9, 1, 5, 3, 6, 2}); //	[-1, 5, 6, 6, -1, -1]
+    // pq!!
+    return 0;
+}
