5ada9aba062ed4cdc345e1b17f72ca44e3e172ee
양원필 <yang-wonphil@Phils-MacBookPro.local>
2023-11-09 16:41:25 +0900

    no_msg

--- programmers/cpp/154539/154539.cpp
+++ programmers/cpp/154539/154539.cpp
@@ -7,8 +7,26 @@ vector<int> solution(vector<int> numbers)
 {
     vector<int> answer;
 
-    // 뒷 큰 수
-
+    // probably 시간초과
+    // for (int i = 0; i < numbers.size(); i++)
+    // {
+    //     int j;
+    //     for (j = i + 1; j < numbers.size(); j++)
+    //     {
+    //         if (numbers[i] < numbers[j])
+    //         {
+    //             answer.push_back(numbers[j]);
+    //             break;
+    //         }
+    //     }
+    //     if (j == numbers.size())
+    //         answer.push_back(-1);
+    // }
+    for (auto &elem : answer)
+    {
+        cout << elem << " ";
+    }
+    cout << "\n";
     return answer;
 }
 
@@ -20,6 +38,7 @@ int main()
 
     solution({2, 3, 3, 5});       //	[3, 5, 5, -1]
     solution({9, 1, 5, 3, 6, 2}); //	[-1, 5, 6, 6, -1, -1]
-    // pq!!
+    // 100만개 max
+    // 풀려면 풀 수 있다. 시간초과가 날뿐
     return 0;
 }
